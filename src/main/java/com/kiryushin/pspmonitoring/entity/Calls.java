4d765ae2f7f43c93a2263438ead5ba137f8bf84e
kiryushinsa <kiryushin.s.a@gmail.com>
2020-05-29 11:20:21 +0300

    Changing unwrap date and time in Entity Calls

--- src/main/java/com/kiryushin/pspmonitoring/entity/Calls.java
+++ src/main/java/com/kiryushin/pspmonitoring/entity/Calls.java
@@ -1,9 +1,13 @@
 package com.kiryushin.pspmonitoring.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 
 @Entity
@@ -29,6 +33,7 @@ public class Calls {
     private TypeOfEmergency type;
 
     @Column(name = "time")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private Date time;
 
     @Column(name = "date")
@@ -44,18 +49,23 @@ public class Calls {
     private String whoAccept;
 
     @Column(name = "time_gone")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private Date time_gone;
 
     @Column(name = "time_arrive")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private Date time_arrive;
 
     @Column(name = "time_local")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private Date time_local;
 
     @Column(name = "time_destroy")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private Date time_destroy;
 
     @Column(name = "time_back")
+    @JsonFormat(pattern="HH:mm",timezone = "GMT+03:00")
     private  Date time_back;
 
     @Column(name = "info_local")
@@ -63,6 +73,15 @@ public class Calls {
 
     @Column(name = "image_url")
     private String image_url;
+
+    @Column(name = "saved")
+    private Integer saved;
+
+    @Column(name = "died")
+    private Integer died;
+
+    @Column(name = "affected")
+    private Integer affected;
 
 
 }
